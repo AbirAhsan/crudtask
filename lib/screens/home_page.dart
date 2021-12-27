287b6cef3e77f963de5407a4f2becf621f1013d2
Abir Ahsan <abirahsan122@gmail.com>
2021-12-27 11:37:36 +0600

    add home screen

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User List"),
+      ),
+      body: SizedBox(
+        height: _height,
+        width: _width,
+      ),
+    );
+  }
+}
